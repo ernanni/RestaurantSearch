84cd85ffc09fa29840b0053948fbde1a3df6feee
Fabio Ernanni <fhenriques@acesso.com>
2020-09-10 02:35:11 -0300

    updating search function parameter

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,22 +7,31 @@ import SearchBar from '../components/SearchBar';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose',
-      },
-    });
-    setResults(response.data.businesses);
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'san jose',
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (e) {
+      setErrorMessage('Error during the request');
+    }
   };
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
-      <Text>Search Screen</Text>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage && <Text>{errorMessage}</Text>}
       <Text>We have found {results.length} results</Text>
     </View>
   );
